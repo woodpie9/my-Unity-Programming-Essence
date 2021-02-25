864241ac445c4a91f38e9ae743b327f0ed764e59
woodpie9 <woodpie9@gmail.com>
2021-02-25 23:39:12 +0900

    11.6.4 Die() 메서드

--- 11/Uni-Run/Assets/Scripts/PlayerController.cs
+++ 11/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -23,10 +23,49 @@ public class PlayerController : MonoBehaviour {
 
    private void Update() {
        // 사용자 입력을 감지하고 점프하는 처리
+       if (isDead)
+       {
+           // 사망 시 처리를 더이상 진행하지 않고 종료
+           return;
+       }
+
+       // 마우스 왼쪽 버튼을 눌렀으며 && 최대 점프 쵯수(2)에 도달하지 않았다면
+       if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+       {
+           // 점프 횟수 증가
+           jumpCount++;
+           // 점프 직전에 속도를 순간적으로 제로(0)로 변경
+           playerRigidbody.velocity = Vector2.zero;
+           // 리지드바디에 위쪽으로 힘 주기
+           playerRigidbody.AddForce(new Vector2(0, jumpForce));
+           // 오디오 소스 재생
+           playerAudio.Play();
+       }
+       else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+       {
+           // 마우스 왼쪽 버튼에서 손을 뗴는 순간 && 속도의 y 값이 양수라면(위로 상승중)
+           // 현재 속도를 절반으로 변경
+           playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+       }
+
+       // 애니메이터의 Grounded 파라미터를 isGrounded 값으로 갱신
+       animator.SetBool("Grounded", isGrounded);
    }
 
    private void Die() {
        // 사망 처리
+       // 애니메이터의 Die 트리거 파라미터를 셋
+       animator.SetTrigger("Die");
+
+       // 오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+       playerAudio.clip = deathClip;
+       // 사망 효과음 재생
+       playerAudio.Play();
+
+       // 속도를 제로(0, 0)로 변경
+       playerRigidbody.velocity = Vector2.zero;
+       // 사망 상태를 true로 변경
+       isDead = true;
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
