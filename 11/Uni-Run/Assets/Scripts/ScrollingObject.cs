b59f2ad82a99e77219fb7319f113549c907869b7
woodpie9 <woodpie9@gmail.com>
2021-02-27 21:17:17 +0900

    ~ Chapter 12
    UI, GameManager

--- 11/Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ 11/Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -6,7 +6,11 @@ public class ScrollingObject : MonoBehaviour {
 
     private void Update() {
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        // 게임 오버가 아니라면
+        if (!GameManager.instance.isGameover)
+        {
         // 초당 speed의 속도로 왼쪽으로 평행 이동
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
